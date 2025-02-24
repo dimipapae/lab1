37debbc3f8651fdc1d706a589b8d519dd5faa9da
ΠΑΠΑΕΥΘΥΜΙΟΥ ΔΗΜΗΤΡΑ <up1092646>
2025-02-24 12:16:23 +0200

    1st question

--- lab1.py
+++ lab1.py
@@ -9,9 +9,13 @@ def more(text):
             reply = input('Show more (y/n)? ')
             if reply == 'n':
                 break
-
+'''
 url = 'http://python.org/'  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
     html = response.text
     more(html)
+'''
+
+url=input("Give url:\t")
+print(url)
